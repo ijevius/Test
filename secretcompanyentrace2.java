ba14a5167f662fb374373013072c5540f9e49d1f
Ruslan Jevik <47752953+ijevius@users.noreply.github.com>
2019-04-23 12:56:50 +0300

    Create secretcompanyentrace2.java

---
+++ secretcompanyentrace2.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class secretcompanyentrace2 {
+
+        public static void main(String[] args){
+
+        Integer[] nums = {}; int l, r;
+        nums = sort(nums);
+
+        int max_l = -1; int min = l;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > min){
+                if(nums[i] < r) {
+                    if (nums[i] - min > max_l) {
+                        max_l = nums[i] - min;
+                        min = nums[i] + 1;
+                    }else{
+                        min = nums[i] + 1;
+                    }
+                }else{
+                    if(nums[i]==r){
+                        if (r - min > max_l) {
+                            max_l = r - min;
+                            min = nums[i] + 1;
+                        }
+                    }else{
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        System.out.println(max_l);
+    }
+
+    static Integer[] sort(Integer[] arr){
+        Arrays.sort(arr, new Comparator<Integer>()
+        {
+            @Override
+            public int compare(Integer x, Integer y){
+                return x-y;
+            }
+        });
+
+        return arr;
+    }
+
+}
