3a6e8cbb714e8e597ec217c3b10eb292ce551672
Ruslan Jevik <47752953+ijevius@users.noreply.github.com>
2019-04-23 13:02:02 +0300

    Update secretcompanyentrace2.java

--- secretcompanyentrace2.java
+++ secretcompanyentrace2.java
@@ -14,8 +14,7 @@ public class secretcompanyentrace2 {
             if(nums[i] > min){
                 if(nums[i] < r) {
                     if (nums[i] - min > max_l) {
-                        max_l = nums[i] - min;
-                        
+                        max_l = nums[i] - min;                        
                     }
                     min = nums[i] + 1;
                 }else{
@@ -27,7 +26,6 @@ public class secretcompanyentrace2 {
                     }else{
                         break;
                     }
-
                 }
             }
         }
@@ -36,8 +34,7 @@ public class secretcompanyentrace2 {
     }
 
     static Integer[] sort(Integer[] arr){
-        Arrays.sort(arr, new Comparator<Integer>()
-        {
+        Arrays.sort(arr, new Comparator<Integer>(){        
             @Override
             public int compare(Integer x, Integer y){
                 return x-y;
