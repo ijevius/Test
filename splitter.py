75875976ace081fad92c9b0028fae4a68ec6b178
Ruslan Jevik <47752953+ijevius@users.noreply.github.com>
2020-03-12 22:35:51 +0300

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -5,9 +5,6 @@ SEPARATOR = " "
 i = 0
 while i < len(s)-2:
     if s[i] == SEPARATOR:
-        if subs != "":
-            print(subs)
-            subs = ""
         i += 1
     else:
         for k in range(i, len(s)):
