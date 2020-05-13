01ae520d6816e60966368897921c45091eb3bbaf
Ruslan <47752953+ijevius@users.noreply.github.com>
2020-05-14 00:25:02 +0300

    Update Leetcode3sum.py

--- Leetcode3sum.py
+++ Leetcode3sum.py
@@ -4,12 +4,12 @@ result = []
 for a in range(len(nums)-2):
     for B in range(len(nums)-1):
         if not B == a:
-            c_exp = -nums[a]-nums[B]
-            c_ins = [i for i,x in enumerate(nums) if x == c_exp]
-            if c_exp in nums and (((not a in c_ins) and (not B in c_ins)) or len(c_ins)>2):
-                result.append(tuple(sorted([nums[a], nums[B], c_exp])))
-            elif (((not a in c_ins) ^ (not B in c_ins))) and len(c_ins)>1:
-                result.append(tuple(sorted([nums[a], nums[B], c_exp])))
+            nums_copy = nums[:]
+            nums_copy[a] = None
+            nums_copy[B] = None
+            c_exp = -nums[a]-nums[B]            
+            if c_exp in nums_copy:                
+                result.append(tuple(sorted([nums[a], nums[B], c_exp])))            
 
 res = [list(block) for block in set(result)]
 print(res)
