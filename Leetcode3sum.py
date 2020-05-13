3f843855262ff85a873a21f8626f857ffd153f9c
Ruslan <47752953+ijevius@users.noreply.github.com>
2020-05-13 20:42:53 +0300

    Update Leetcode3sum.py
    I have no idea why leetcode doesnt accept solve [[-2,1,1]] for input [1,2,-2,-1]

--- Leetcode3sum.py
+++ Leetcode3sum.py
@@ -1,13 +1,12 @@
-#TODO optimize it 
-
 nums = []
 result = []
 
 for a in range(len(nums)-2):
     for B in range(len(nums)-1):
         if not B == a:
-            for c in range(len(nums)):
-                if (not c == a and not c == B) and (nums[a]+nums[B]+nums[c] == 0):
-                    result.append(tuple(sorted([nums[a], nums[B], nums[c]])))                    
+            c_exp = -nums[a]-nums[B]
+            if c_exp in nums:
+                result.append(tuple(sorted([nums[a], nums[B], c_exp])))
 
-print([list(block) for block in set(result)])
+res = [list(block) for block in set(result)]
+print(res)
